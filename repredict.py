0566352f1011a2780aee07d3e21e037040f31c27
XZLeo <13718722639@163.com>
2020-07-28 10:58:37 +0800

    enter

--- repredict.py
+++ repredict.py
@@ -11,10 +11,10 @@ from keras.applications.mobilenet import preprocess_input, decode_predictions
 with CustomObjectScope({'relu6': relu6,'DepthwiseConv2D': DepthwiseConv2D}):
     model = load_model("D:\\webAI\\cats_dog_mobileNet.h5")
 
-#print(model.summary())
+print(model.summary())
 
 #加载单个图片用于预测
-img_path = 'C:\\Users\\13718\\.keras\\datasets\\dogs-vs-cats\\train\\cat.3901.jpg' #cat为0，dog为1
+img_path = 'C:\\Users\\13718\\Desktop\\mmexport1595488605310.jpg' #cat为0，dog为1
 img = image.load_img(img_path, target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
@@ -25,4 +25,4 @@ if 0 < preds < 0.5:
     print("目标预测为猫，概率为{}%".format((1-preds)*100))
 elif preds > 0.5:
     print("目标预测为狗，概率为{}%".format((preds)*100))
-# 概率最大的三个预测
+# # 概率最大的三个预测
