e0b60e860d1befc1e7aaeb0f23e91e4407c3f49e
XZLeo <13718722639@163.com>
2020-07-30 15:13:20 +0800

    enter

--- TryMobile.py
+++ TryMobile.py
@@ -8,76 +8,65 @@ import numpy as np
 import time
 from plotAccLoss import plotLossAcc
 
-#迁移训练模型
-base_model = MobileNet(input_shape=(224, 224, 3),weights='imagenet', include_top=False) #channel_last
-for layer in base_model.layers:
-    layer.trainable=False
-# print(base_model.summary())
-#没有top是去掉了最后的GAP层和Softmax层
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)
-#添加类别分类器，只有两类
-classifier = Dense(1, activation='sigmoid')(x)
-model = Model(inputs=base_model.input, outputs=classifier)
+def reTrain():
+    #迁移训练模型
+    base_model = MobileNet(input_shape=(224, 224, 3),weights='imagenet', include_top=False) #channel_last
+    for layer in base_model.layers:
+        layer.trainable=False
+    # print(base_model.summary())
+    #没有top是去掉了最后的GAP层和Softmax层
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    #添加类别分类器，只有两类
+    classifier = Dense(1, activation='sigmoid')(x)
+    model = Model(inputs=base_model.input, outputs=classifier)
 
-#文件路径
-import os, shutil
-original_dataset_dir = 'C:/Users/13718/.keras/datasets/dogs-vs-cats/train'
-base_dir = 'C:/Users/13718/.keras/datasets/cats_and_dogs_small' # save small dataset
-train_dir = os.path.join(base_dir, 'train')
-validation_dir = os.path.join(base_dir, 'validation')
-test_dir = os.path.join(base_dir, 'test')
+    #文件路径
+    import os, shutil
+    original_dataset_dir = 'C:/Users/13718/.keras/datasets/dogs-vs-cats/train'
+    base_dir = 'C:/Users/13718/.keras/datasets/cats_and_dogs_small' # save small dataset
+    train_dir = os.path.join(base_dir, 'train')
+    validation_dir = os.path.join(base_dir, 'validation')
+    test_dir = os.path.join(base_dir, 'test')
 
-#使用生成器读取图像
-train_datagen = ImageDataGenerator(rescale=1./255) #所有像素缩小255倍
-test_datagen = ImageDataGenerator(rescale=1./255) 
+    #使用生成器读取图像
+    train_datagen = ImageDataGenerator(rescale=1./255) #所有像素缩小255倍
+    test_datagen = ImageDataGenerator(rescale=1./255) 
 
-train_generator = train_datagen.flow_from_directory(
-    train_dir,               #不同分类对应不同子目录
-    target_size=(224, 224), 
-    batch_size=20,
-    class_mode='binary') #使用二进制标签
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,               #不同分类对应不同子目录
+        target_size=(224, 224), 
+        batch_size=20,
+        class_mode='binary') #使用二进制标签
 
-validation_generator = test_datagen.flow_from_directory(
-    validation_dir,               
-    target_size=(224, 224),  # 将图像调整为150*150
-    batch_size=20,
-    class_mode='binary') 
+    validation_generator = test_datagen.flow_from_directory(
+        validation_dir,               
+        target_size=(224, 224),  # 将图像调整为150*150
+        batch_size=20,
+        class_mode='binary') 
 
-model.compile(loss='binary_crossentropy',
-             optimizer='rmsprop', #手动设置学习率，默认的是
-             metrics=['acc'])    
+    model.compile(loss='binary_crossentropy',
+                optimizer='rmsprop', #手动设置学习率，默认的是
+                metrics=['acc'])    
 
-begin = time.time()
-#重新训练分类器
-history = model.fit_generator(
-        train_generator,
-        steps_per_epoch=100,
-        epochs=5,
-        validation_data=validation_generator,
-        validation_steps=50)
-print("trainning time {}".format(time.time()-begin))
-# trainning time 2093.084360599518
-#训练完应保存权重
-model.save('cats_dog_mobileNet.h5')
+    begin = time.time()
+    #重新训练分类器
+    history = model.fit_generator(
+            train_generator,
+            steps_per_epoch=100,
+            epochs=5,
+            validation_data=validation_generator,
+            validation_steps=50)
+    print("trainning time {}".format(time.time()-begin))
+    # trainning time 2093.084360599518
+    #训练完应保存权重
+    model.save('.\static\cats_dog_mobileNet.h5')
 
-plotLossAcc(history)
+    plotLossAcc(history)
 
-
-
-
-
-#加载单个图片用于预测
-# img_path = 'C:\\Users\\13718\\.keras\\datasets\\dogs-vs-cats\\train\\dog.3901.jpg'
-# img = image.load_img(img_path, target_size=(224, 224))
-# x = image.img_to_array(img)
-# x = np.expand_dims(x, axis=0)
-# x = preprocess_input(x)
-
-# preds = model.predict(x)
-# print('Predicted:', decode_predictions(preds, top=3)[0])
-# 概率最大的三个预测
-
-
-
+#似乎必须用data_generator加载图片
+if __name__ == "__main__":
+    reTrain()
+# 开始训练的cpu占用率达到100%
+# 8核cpu
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template, request, flash, make_response
+from flask import Flask, render_template, request, flash, make_response, redirect, url_for
 from werkzeug.utils import secure_filename
 from PIL import Image
 from repredict import repredict
+from shutil import move
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456'
@@ -29,10 +30,10 @@ def upload_file():
     if request.method == 'POST':
         #获取上传文件
         file = request.files['file']
-        # print(file.filename)
+        print(dir(file))
         #检查文件对象是否存在且合法
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename) #把汉字文件名抹掉了.....
+        if file and allowed_file(file.filename): #哪里规定file都有什么属性
+            filename = secure_filename(file.filename) #把汉字文件名抹掉了，所以下面多一道检查
             if filename != file.filename:
                flash("only support ASCII name")
                return render_template('upload.html')            
@@ -42,28 +43,63 @@ def upload_file():
             except FileNotFoundError:
                 os.mkdir(UPLOAD_FOLDER)
                 file.save(os.path.join(UPLOAD_FOLDER, filename))
-            return render_photo_as_page(filename)
+            return redirect(url_for('update', fileName=filename))
         else:
             return 'Upload Failed'
     else: #GET方法
         return render_template('upload.html')
 
 def render_photo_as_page(filename):
-    """每次调用都重写static/images/test.png"""
+    """每次调用都将上传的图片复制到static中"""
     img = Image.open(os.path.join(UPLOAD_FOLDER, filename))  #上传文件夹和static分离
     img.save(os.path.join('./static/images', filename)) #这里要求jpg还是png都必须保存成png，因为html里是写死的
+    #predict
     preds = repredict(filename)
-    return render_template('show.html', fname='images/'+filename, preds=preds)
-    #flask要求视图函数必须返回字符串或者模板
+    result = {}
+    result["prediction"] = preds[0]
+    result["probability"] = preds[1]
+    result["fileName"] =  filename
+    return result     
+
+@app.route('/upload/<path:fileName>', methods=['POST', 'GET'])
+def update(fileName):
+    """输入url加载图片，并返回预测值；上传图片，也会重定向到这里"""
+    result = render_photo_as_page(fileName) 
+    return render_template('show.html', fname='images/'+fileName, result=result)
+
+@app.route('/thanks', methods=['POST', 'GET'])
+def thanks():
+    print("2\n")
+    category = request.form["feedback_button"] #True or False
+    x = {"Cat", "Dog"}
+    if category == 'Incorrect':
+        result["prediction"] = x - {result["prediction"]} #猫变狗，狗变猫
+    src = './static/{}'.format(fileName)
+    dst = './static/images/{}'.format(result["prediction"])
+    try:
+        move(src, dst)
+    except FileNotFoundError:
+        os.mkdir(dst)
+        move(src, dst)           #在这些函数中train的话，会变得特别慢，能单独开一个进程么
+    #调用update去更新模型
+    return render_template('thanks.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 #用secure_filename获取中文文件名时,中文会被省略。
 #原因：secure_filename()函数只返回ASCII字符，非ASCII字符会被过滤掉
-#安装pypinyin包，将汉字文件名转换成拼音
+#解决方案；安装pypinyin包，将汉字文件名转换成拼音
 
 #flash是要在上传页面配合模板的get_flash使用的
+
+#flask要求视图函数必须返回字符串或者模板
+
+#redirect不能传值，但url_for可以传值
+
+#predict和trian的过程都太慢，需要异步请求？
+#异步请求or多线程？
 
 
 
